41e1aeb8df8b716f5bd25dd14ffee7841be11d3f
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-07-06 11:59:05 +0100

    folder structure changed to individual component css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+
+import './App.css'
+import { useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import HeroSection from './components/hero/HeroSection';
+import StoreSection from './components/storesection/StoreSection';
+import Footer from './components/footer/Footer';
+import Subscribe from './components/subscription/Subscribe';
+const App = () => {
+  const [alert, setAlert] = useState("")
+  const handleClick = ()=>{
+    let message = 'please check your email!';
+    setAlert(message)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Navbar/>
+    <HeroSection alert={alert} handleClick={handleClick}/>
+    <StoreSection/>
+    <Subscribe alert={alert} handleClick={handleClick}/>
+    <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../../images/logo.svg';
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='top-nav'>
+     <nav>
+        <img src={Logo} alt="" />
+        <ul>
+          <li><a href="/">Features</a></li>
+          <li><a href="/">Team</a></li>
+          <li><a href="/">Sign In</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
